04b1844b240d7de57ef040c320868688cfc37cb7
Kevin BRUNO <kevinbruno90@gmail.com>
2022-01-20 12:20:01 +0100

    sliderwashingmachin component works in mobile first

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,10 @@
 import './App.css';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import SwiperCore, { Navigation } from 'swiper';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import lavelinge2 from './assets/lavelinge2.png';
-import lavelinge3 from './assets/lavelinge3.png';
-
-SwiperCore.use([Navigation]);
+import SliderWashingMachine from './components/slider/SliderWashingMachine';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello DIYMCA</h1>
-      <Swiper
-      modules={[Navigation]}
-      spaceBetween={1}
-      slidesPerView={1}
-      slidesPerGroup={1}
-      navigation
-    >
-      <SwiperSlide></SwiperSlide>
-      <SwiperSlide><img src={lavelinge2} alt="lavelinge" /></SwiperSlide>
-      <SwiperSlide></SwiperSlide>
-      <SwiperSlide><img src={lavelinge3} alt="lavelinge" /></SwiperSlide>
-    </Swiper>
+      <SliderWashingMachine />
     </div>
   );
 }
